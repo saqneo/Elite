859de264594d575526c0995093020059bca3fed6
saqneo <saqneo@gmail.com>
2016-02-15 10:34:21 -0800

    Update Install-ElitePaddles.ps1

--- Elite/Install-ElitePaddles.ps1
+++ Elite/Install-ElitePaddles.ps1
@@ -1,9 +1,8 @@
-﻿param
+param
 (
     [String]
     $SlnPath = "Elite.sln"
 )
-
 
 $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = New-Object Security.Principal.WindowsPrincipal -ArgumentList $identity
@@ -18,7 +17,7 @@ Running this script will perform the following actions on your PC:
 2. Compile Elite.sln. It is expected that the projects were not modified from how they were packeged with the solution.
 3. Register the EliteService which will run in the background on the machine as an HTTP listener on port 8642 (http://localhost:8642/EliteService)
 4. Register the above url for the active user. Existing registration will be removed as it is assumed to be stale state.
-5. Generate certificate to sign the appx package. The user will be prompted to create passwords for the certs, and then again to use them (4 prompts).
+5. Generate certificate to sign the appx package. The user will be prompted for passwords to create the certs, and then again to use them (4 prompts).
 6. Add the certificate to the root store and sign the appx package.
 7. Deploy the appx package.
 "@
@@ -74,7 +73,7 @@ $xml.Project.ItemGroup.Reference | ? { $_.HintPath -and $_.HintPath.Contains("Xb
 $xml.Save($eliteUiCsprojLocation)
 
 # Build the solution
-& $msbuildLocation $SlnPath /t:Rebuild "/p:Configuration=Release;OutDir=.\Out\;Platform=x64;AppxPackageSigningEnabled=false"
+& $msbuildLocation $SlnPath /m:4 /t:Rebuild "/p:Configuration=Release;OutDir=.\Out\;Platform=x64;AppxPackageSigningEnabled=false"
 
 # This check should not fail since the exe was built above, but check the EliteService.exe is in its expected path
 $eliteServiceLocation = $slnDir + "\EliteService\Out\EliteService.exe"
@@ -125,4 +124,4 @@ Copy-item -Path $cerPath -Destination $eliteLocation -Force
 & $signtoolLocation sign /fd SHA256 /a /f $pfxPath /p "ElitePaddles_TestKeyPw" $eliteAppxLocation
 
 # Install the package
-[ScriptBlock]::Create($eliteAppxAddLocation).Invoke()
+Invoke-Expression "& '$eliteAppxAddLocation'"
